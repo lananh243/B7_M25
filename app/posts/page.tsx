8b3d11484a86501d4bfa7373a461d4ce25f465cc
lan anh234lan anh 234 <nguyenthilananh21092005@gmail.com>
2024-09-04 15:49:36 +0700

    B7

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -2,7 +2,7 @@
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
-export default function page() {
+export default function Page() {
   const [value, setValue] = useState<any>();
   const router = useRouter();
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
